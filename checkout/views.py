d81eea4fdf843d1d3ccd910c8677fbdaade7cb44
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-01-16 13:23:44 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QhrWHGLG50LDIYR35Z8xzuA6aCINdqsFqB5C4lN6y2gHNxu9xJgRh8xcfDDge2DEE7zvd3CR63TCDxZUNnUG7lN00uqpmkneT',
+        'client_secret': 'test client_secret',
     }
 
     return render(request, template, context)
